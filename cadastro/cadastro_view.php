e4cd9ea3031c59d3c616a98672c981e2014a74bc
Alessandra Soares <alessandra.prodv@gmail.com>
2013-04-26 09:31:11 -0300

    Revert "Revert "Revert "Revert "Cadastro""""
    This reverts commit 23cc07461129328765dec8f4e60b54b02791ecce.

--- cadastro/cadastro_view.php
+++ cadastro/cadastro_view.php
@@ -1,27 +1,16 @@
-<?xml version="1.0" encoding="utf-8"?>
-<!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Transitional//EN" "http://www.w3.org/TR/xhtml1/DTD/xhtml1-transitional.dtd">
-   <html xmlns="http://www.w3.org/1999/xhtml" lang="pt-br" xml:lang="pt-br">
-       <head>
-       		<meta http-equiv="Content-Type" content="text/html; charset=utf-8" />
-			<title>Modelo Transitional UTF</title>	
-			    <link rel="stylesheet" type="text/css" href="estilos_photoshoot.css"> 
-				<style type="text/css">
-				  @import url(css/estilos_photoshoot.css);
-				</style>
-       </head>
-       <body>
+<? include head; ?>
 	
     <!-- Principal -->
 	<div id="conteudo"> <div id="barra_titulo"></div>
 		<!-- Caixa da esquerda -->
 		<div id="cad_esquerda">
-			 <a href="inicio.php"><img src="images/Logo_cadastro.png" alt="" /></a>
+			 <a href="inicio"><img src="<? echo DEFAULT_URL.IMG.""; ?>Logo_cadastro.png" alt="" /></a>
 		</div>
 		<div id="cad_direita">
-        <img src="images/photos.png" alt="" />
+        <a href="inicio"><img src="<? echo DEFAULT_URL.IMG.""; ?>photos.png" alt="" /></a>
         </div>
 		
-		
+		<? if(isset($_POST['cadastro'])){$post = $class->Post(); $cadastro = $class->Cadastrar(); }?>
         
 		<div class="clear"></div>
         <div id="barra-titulo"></div>
@@ -31,17 +20,18 @@
                 <div class="titulo_c">Cadastre-se</div>
                 <div class="texto_c">Registe-se gratuitamente e compartilhe<br>seus momentos!</div>
                   <div class="engloba_form">
-                    <form method="post" action="">
+                   <form id="form" method="post" action="" enctype="multipart/form-data">
                         Nome<br> 
                         <input type="text" autocomplete="off" name="nome" class="campo_1"  value="Nome" onfocus="if(this.value='Nome') this.value='';" onBlur="if(this.value=='') this.value='Nome';">
                         <input type="text" autocomplete="off" name="sobrenome" class="campo_1" value="Sobrenome" onfocus="if(this.value='Sobrenome') this.value='';" onBlur="if(this.value=='') this.value='Sobrenome';">
-                        <br>Nome de Usuário<br>
-                        <input type="text" autocomplete="off" name="login" class="campo_1">
+                        <br>Login<br>
+                        <input type="text" autocomplete="off" name="usuario" class="campo_1">
                         <br>Crie uma senha<br>
                         <input type="password" autocomplete="off" name="senha" class="campo_2">
                         <br>Confirme sua senha<br>
                         <input type="password" autocomplete="off" name="confirmacao" class="campo_2">
-                        <a href="perfil.php"><input type="button" name="entrar" id="botao_cadastro" value="Continuar"></a>
+                       
+                        <input type="submit" name="cadastro" id="botao_cadastro" value="Continuar" onclick="return validar();">
                     </form>
                   </div>  
                 </div>
@@ -70,16 +60,4 @@
 	</div>
     
 	
-	<footer>
-		<ul>
-			<li><a href="#">Sobre nós</a></li>
-			<li><a href="#">Suporte</a></li>
-			<li><a href="#">Privacidade</a></li>
-			<li><a href="#">Termos</a></li>
-			<li><a href="#">Desenvolvedores</a></li>
-			<li>&copy; 2013 Photoshoot</li>
-		</ul>
-	</footer>
-	
-</body>
-</html>
+<? include foot; ?>
